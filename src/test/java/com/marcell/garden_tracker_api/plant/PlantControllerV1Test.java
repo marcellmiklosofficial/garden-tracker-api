6ea7589c0dc8fd797c89d8bca61d0bb91d3b6358
Marcell Miklos <marcell.miklos@gmail.com>
2026-02-03 16:02:18 +0100

    Use H2 in-memory database for tests with test profile

--- src/test/java/com/marcell/garden_tracker_api/plant/PlantControllerV1Test.java
+++ src/test/java/com/marcell/garden_tracker_api/plant/PlantControllerV1Test.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class PlantControllerV1Test {
     @Autowired
     MockMvc mockMvc;
